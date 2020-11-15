38379b547e109d0eaa584136023391e3157e2e42
Sajeewa Dilshan <sajeewadilshansl@gmail.com>
2020-11-16 04:11:08 +0530

    Modify js File

--- js/customer-ctrl.js
+++ js/customer-ctrl.js
@@ -55,6 +55,7 @@ function init(){
 /*   cusTable.deleteRow(1);*/
 
 var cusTable=document.getElementById("tbl-customers");
+/*
 var tr=cusTable.insertRow(1);
 var td=document.createElement("td");
 td=tr.insertCell(0);
@@ -66,7 +67,7 @@ delb.setAttribute("class","rec");
 
 delb.innerText="sdfsdfasf";
 td=tr.insertCell(1);
-td.appendChild(delb);
+td.appendChild(delb);*/
 
 var remButton=document.getElementById("rem-button");
 
@@ -89,17 +90,112 @@ remButton.addEventListener("click", function getInputValue(){
     alert(txtid);
 });
 
+var txtIdElement=document.getElementById("txt-id");
 
+function validateInput(){
+    // Selecting the input element and get its value
+    var txtid = document.getElementById("txt-id").value;
+    var txtName = document.getElementById("txt-name").value;
+    var txtAddress=document.getElementById("txt-address").value;
+
+
+
+    if(/([C])\d{3}$/.test(txtid)){
+
+    }else{
+        txtIdElement.focus();
+        alert("Please enter a valid ID");
+        return  false;
+
+
+    }
+
+    if(/[[a-zA-Z.]{3,}[a-zA-Z. ]/.test(txtName)){
+
+    }else{
+        txtIdElement.focus();
+        alert("Please enter a valid Name");
+        return  false;
+    }
+
+
+    if(/[a-z|A-Z|0-9.| |,|/]/.test(txtAddress) ){
+        console.log(txtAddress);
+
+
+    }else{
+        alert("Please enter a valid address");
+        return  false;
+    }
+
+
+}
+
+function saveDetails(){
+    if(validateInput()===false){
+        alert("validation failed");
+
+    }else{
+
+        var txtid = document.getElementById("txt-id").value;
+        var txtName = document.getElementById("txt-name").value;
+        var txtAddress=document.getElementById("txt-address").value;
+
+        var inputData=[txtid,txtName,txtAddress];
+
+
+        var tr=cusTable.insertRow(cusTable.rows.length);
+
+
+        var td=document.createElement("td");
+        td=tr.insertCell(0);
+        td.innerText=inputData[0];
+
+        alert("input data" +inputData[0]);
+        td=tr.insertCell(1);
+        td.innerText=inputData[1];
+
+        td=tr.insertCell(2);
+        td.innerText=inputData[2];
+
+
+
+
+    }
+    alert("saved detail executed");
+
+
+
+
+}
+
+
+saveButton.addEventListener("click",function (){
+
+    var tableRow
+
+
+
+});
+
+
+
+
+/*
 saveButton.addEventListener("click", function getInputValue(){
     // Selecting the input element and get its value
     var txtid = document.getElementById("txt-id").value;
 
     if(/([C])\d{3}/.test(txtid)){
-        alert("success");
+
+    }else{
+        alert("Please enter a valid ID");
+
     }
 
     alert(txtid);
 });
+*/
 
 
 
@@ -117,3 +213,4 @@ remButton.addEventListener("mouseleave",function (){
 
 
 });
+
