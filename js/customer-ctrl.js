54522f6860e45eb0b71878f759439cba04d2d989
Sajeewa Dilshan <sajeewadilshansl@gmail.com>
2020-11-16 02:23:04 +0530

    Configure the file

--- js/customer-ctrl.js
+++ js/customer-ctrl.js
@@ -14,20 +14,25 @@
  *   0. You just DO WHAT THE FUCK YOU WANT TO.
  */
 
+
 /**
  * @author : Ranjith Suranga <suranga@ijse.lk>
  * @since : 11/15/20
  **/
 
+
 /*===============================================================================
  * Global Variables
  *===============================================================================*/
 
+
 // Todo: add all global variable declaration here
+
 
 /*===============================================================================
  * Init
  *===============================================================================*/
+
 
 init();
 
@@ -46,3 +51,52 @@ function init(){
  *===============================================================================*/
 
 // Todo: add all functions
+/*([C])\d{3}*/
+/*   cusTable.deleteRow(1);*/
+
+var cusTable=document.getElementById("tbl-customers");
+var tr=cusTable.insertRow(1);
+var td=document.createElement("td");
+td=tr.insertCell(0);
+td.innerText="agafgdafg";
+
+var delb=document.createElement("button");
+delb.setAttribute("value","delete");
+delb.setAttribute("class","rec");
+
+delb.innerText="sdfsdfasf";
+td=tr.insertCell(1);
+td.appendChild(delb);
+
+var remButton=document.getElementById("rem-button");
+
+
+
+
+
+
+
+remButton.addEventListener("click", function getInputValue(){
+    // Selecting the input element and get its value
+    var inputVal = document.getElementById("txt-id").value;
+
+    if(/([C])\d{3}/.test(inputVal)){
+        alert("success");
+    }
+
+    alert(inputVal);
+});
+
+remButton.addEventListener("mouseenter",function (){
+   remButton.style.backgroundColor="red";
+});
+
+
+
+
+
+remButton.addEventListener("mouseleave",function (){
+    remButton.style.backgroundColor="purple";
+    console.log("safs"+inp);
+
+});
